09404240fe9aa5ff720d4e41d90b3ba41c646252
Luciano Alves <lasbr_alves@outlook.com>
2024-01-30 10:41:13 -0300

    feat: added comments to posts.

--- src/main/java/br/com/lasbr/socialhub/config/Instantiation.java
+++ src/main/java/br/com/lasbr/socialhub/config/Instantiation.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Configuration;
 import br.com.lasbr.socialhub.domain.Post;
 import br.com.lasbr.socialhub.domain.User;
 import br.com.lasbr.socialhub.dto.AuthorDTO;
+import br.com.lasbr.socialhub.dto.CommentDTO;
 import br.com.lasbr.socialhub.repositories.PostRepository;
 import br.com.lasbr.socialhub.repositories.UserRepository;
 
@@ -42,6 +43,13 @@ public class Instantiation implements CommandLineRunner {
 		
 		Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
 		Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO(maria));
+		
+		CommentDTO comment1 = new CommentDTO("Boa viagem, Maria!", sdf.parse("21/03/2018"), new AuthorDTO(alex));
+		CommentDTO comment2 = new CommentDTO("Aproveite", sdf.parse("22/03/2018"), new AuthorDTO(bob));
+		CommentDTO comment3 = new CommentDTO("Tenha um ótimo dia!", sdf.parse("23/03/2018"), new AuthorDTO(alex));
+		
+		post1.getComments().addAll(Arrays.asList(comment1, comment2));
+		post2.getComments().addAll(Arrays.asList(comment3));
 		
 		postRepository.saveAll(Arrays.asList(post1, post2));
 		
