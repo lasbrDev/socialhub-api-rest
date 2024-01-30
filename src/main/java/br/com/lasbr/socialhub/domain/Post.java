fc98ca84ab1e6d93c02d34166f2f0038917bee3d
Luciano Alves <lasbr_alves@outlook.com>
2024-01-30 10:41:13 -0300

    feat: added comments to posts.

--- src/main/java/br/com/lasbr/socialhub/domain/Post.java
+++ src/main/java/br/com/lasbr/socialhub/domain/Post.java
@@ -1,13 +1,16 @@
 package br.com.lasbr.socialhub.domain;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import br.com.lasbr.socialhub.dto.AuthorDTO;
+import br.com.lasbr.socialhub.dto.CommentDTO;
 
 @Document
 public class Post implements Serializable {
@@ -21,6 +24,8 @@ public class Post implements Serializable {
 	private String body;
 	
 	private AuthorDTO author;
+	
+	private List<CommentDTO> comments = new ArrayList<>();
 	
 	public Post() {
 	}
@@ -72,6 +77,10 @@ public class Post implements Serializable {
 	
 	public void setAuthor(AuthorDTO author) {
 		this.author = author;
+	}
+	
+	public List<CommentDTO> getComments() {
+		return comments;
 	}
 
 	@Override
