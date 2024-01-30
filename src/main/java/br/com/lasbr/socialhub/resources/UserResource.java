c91d0850717c66a64bbdf30a2cd1d12a5604b5b6
Luciano Alves <lasbr_alves@outlook.com>
2024-01-29 22:14:03 -0300

    feat: added method to update user PUT.

--- src/main/java/br/com/lasbr/socialhub/resources/UserResource.java
+++ src/main/java/br/com/lasbr/socialhub/resources/UserResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -52,6 +53,14 @@ public class UserResource {
 	@DeleteMapping("/{id}")
 	public ResponseEntity<Void> delete(@PathVariable String id) {
 		service.delete(id);
+		return ResponseEntity.noContent().build();
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+		User user = service.fromDTO(objDto);
+		user.setId(id);
+		service.update(user);
 		return ResponseEntity.noContent().build();
 	}
 }
