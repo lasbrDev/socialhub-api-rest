9b85fcbf35508053b896a831271b7878d017fd7f
Luciano Alves <lasbr_alves@outlook.com>
2024-01-29 21:32:38 -0300

    feat: added post method to create user.

--- src/main/java/br/com/lasbr/socialhub/resources/UserResource.java
+++ src/main/java/br/com/lasbr/socialhub/resources/UserResource.java
@@ -1,12 +1,16 @@
 package br.com.lasbr.socialhub.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.lasbr.socialhub.domain.User;
 import br.com.lasbr.socialhub.dto.UserDTO;
@@ -35,5 +39,13 @@ public class UserResource {
 		UserDTO dto = new UserDTO(user);
 		return ResponseEntity.ok().body(dto);
 	}	
+	
+	@PostMapping
+	public ResponseEntity<Void> insert(@RequestBody UserDTO objDto) {
+		User user = service.fromDTO(objDto);
+		user = service.insert(user);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{id}").buildAndExpand(user.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
 	
