00a9c4600987e0b698e2eecbdbd3f0b2e8971756
Luciano Alves <lasbr_alves@outlook.com>
2024-01-30 21:07:22 -0300

    fix: merge and fix

---
+++ src/main/java/br/com/lasbr/socialhub/SocialhubApplication.java
@@ -0,0 +1,12 @@
+package br.com.lasbr.socialhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialhubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialhubApplication.class, args);
+	}
+}
---
+++ src/test/java/br/com/lasbr/socialhub/SocialhubApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lasbr.socialhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialhubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
